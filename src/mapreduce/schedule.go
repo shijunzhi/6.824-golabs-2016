e229a7edf4769eb0d63069c2379d49d9f6c5bbc9
jushi <jushi@localhost.localdomain>
2017-01-15 19:31:34 +0800

    implement lab 1 part III

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -24,5 +24,64 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	var works = make([]string, 0)
+	var scheduleChannel = make(chan *DoTaskArgs, ntasks)
+	var failureChannel = make(chan *DoTaskArgs)
+	var doneTasks = 0
+	var taskArgs *DoTaskArgs
+	for i := 0; i < ntasks; i++ {
+		taskArgs = new(DoTaskArgs)
+		taskArgs.JobName = mr.jobName
+		taskArgs.Phase = phase
+		taskArgs.TaskNumber = i
+		taskArgs.NumOtherPhase = nios
+		if phase == mapPhase {
+			taskArgs.File = mr.files[i]
+		}
+
+		scheduleChannel <- taskArgs
+	}
+
+	for doneTasks < ntasks {
+		select {
+		case workAddr := <-mr.registerChannel:
+			works = append(works, workAddr)
+			go mr.scheduleTask(workAddr, scheduleChannel, failureChannel)
+		case failureTask := <-failureChannel:
+			if failureTask == nil {
+				doneTasks++
+			} else {
+				scheduleChannel <- failureTask
+			}
+		}
+	}
+	close(scheduleChannel)
+
+	if phase == mapPhase {
+		go func() {
+			for _, workAddr := range works {
+				mr.registerChannel <- workAddr
+			}
+		}()
+	}
+
 	fmt.Printf("Schedule: %v phase done\n", phase)
+}
+
+func (mr *Master) scheduleTask(workAddr string, scheduleChannel chan *DoTaskArgs,
+	failureChannel chan *DoTaskArgs) {
+	for {
+		taskArgs, open := <-scheduleChannel
+		if open {
+			ok := call(workAddr, "Worker.DoTask", taskArgs, new(struct{}))
+			if ok {
+				failureChannel <- nil
+			} else {
+				fmt.Printf("run task %d on worker %s failed\n", taskArgs.TaskNumber, workAddr)
+				failureChannel <- taskArgs
+			}
+		} else {
+			break
+		}
+	}
 }
